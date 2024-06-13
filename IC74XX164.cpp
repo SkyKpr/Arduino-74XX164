ec3abde681ed3fdbd12a4db0c2cf5d970c9f974b
M.Khlmann <tobeedited@edit.com>
2024-06-13 09:51:03 +0200

    shiftBits function added

--- IC74XX164.cpp
+++ IC74XX164.cpp
@@ -1,34 +1,51 @@
 #include <Arduino.h>
+#include <stdio.h>
 #include "IC74XX164.h"
 
-IC74XX164::IC74XX164(uint8_t dP, uint8_t clkP, uint8_t clrP, uint8_t eP) {
-  dataPin = dP;
+IC74XX164::IC74XX164(uint8_t dataP, uint8_t clkP, uint8_t clrP) {
+  dataPin = dataP;
   clockPin = clkP;
   clearPin = clrP;
-  enablePin = eP;
 }
 
 IC74XX164::begin(void) {
+  
   pinMode(dataPin, OUTPUT);
   pinMode(clockPin, OUTPUT);
   pinMode(clearPin, OUTPUT);
-  pinMode(enablePin, OUTPUT);
 
   digitalWrite(dataPin, LOW);
   digitalWrite(clockPin, LOW);
   digitalWrite(clearPin, HIGH);
-  digitalWrite(enablePin, LOW);
 }
 
-IC74XX164::shiftLSB(uint8_t value) {
-  
-  digitalWrite(enablePin, HIGH);
-
+IC74XX164::shift8Bit(uint8_t value) {
   for(int i = 0; i < 8; i++) {
     digitalWrite(dataPin, value & (1 << i));
     digitalWrite(clockPin, HIGH);
     digitalWrite(clockPin, LOW);
   }
+}
 
-  digitalWrite(enablePin, LOW);
+IC74XX164::shiftBits(uint16_t value, uint8_t lenght) {
+  for(int i = 0; i < lenght; i++) {
+    digitalWrite(dataPin, (bool)(value & (1 << i)));
+    digitalWrite(clockPin, HIGH);
+    digitalWrite(clockPin, LOW);
+  }
+}
+
+IC74XX164::clockPulse(uint8_t number) {
+  digitalWrite(registers[number], HIGH);
+
+  digitalWrite(clockPin, HIGH);
+  digitalWrite(clockPin, LOW);
+
+  digitalWrite(registers[number], LOW);
+}
+
+IC74XX164::clear(void) {
+  digitalWrite(clearPin, LOW);
+  delay(10);
+  digitalWrite(clearPin, HIGH);
 }
--- IC74XX164.h
+++ IC74XX164.h
@@ -5,14 +5,17 @@
 
 class IC74XX164 {
   public:
-    IC74XX164(uint8_t dataPin, uint8_t clockPin, uint8_t clearPin, uint8_t enablePin);
+    IC74XX164(uint8_t dataP, uint8_t clkP, uint8_t clrP);
     begin(void);
-    shiftLSB(uint8_t value);
+    shift8Bit(uint8_t value);
+    shiftBits(uint16_t value, uint8_t lenght);
+    clockPulse(uint8_t  number);
+    clear(void);
 
     uint8_t dataPin;
     uint8_t clockPin;
     uint8_t clearPin;
-    uint8_t enablePin;
+    uint8_t registers[6];
 };
 
 #endif
